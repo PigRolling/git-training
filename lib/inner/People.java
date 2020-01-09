603203a823dc75a5c10984ff1824f1f6b19c2551
CRX <1244073968@qq.com>
2020-01-09 18:38:18 +0800

    添加了一个文件夹及相关文件

---
+++ lib/inner/People.java
@@ -0,0 +1,16 @@
+package com.crx.work;
+
+public class People {
+
+	private String name;
+
+	private int age;
+
+	private String sex;
+
+	public People(String name,int age,String sex){
+		this.name = name;
+		this.age = age;
+		this.sex = set;
+	}
+}
